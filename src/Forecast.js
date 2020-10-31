7141f209424b821ad7345852249da2267d0f5527
pras75299 <singh.prashantking@gmail.com>
2020-10-31 17:17:15 +0530

    download bg image, add forcast component css

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,31 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import "./Forecast.css"
+const API_KEY = "e1e3aeef1a428610073ff9107da4bbf5"
+const API_URL = `https://pro.openweathermap.org/data/2.5/forecast/climate?q={London},{GB}&appid=${API_KEY}`
 
 const Forecast = () => {
-   let [responseObj, setResponseObj] = useState({}); 
-   function getForecast() {
-    fetch("https://community-open-weather-map.p.rapidapi.com/climate/month?q=seattle", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-            "x-rapidapi-key": "2e3858cdb0mshf8069e22b020ca0p179dc1jsn973a3298831f"
-        }
-    })
-    .then(response => response.json())
-    .then(response => {
-        setResponseObj(response)
-    })
-    .catch(err => {
-        console.log(err);
-    });
-   }
+  let [responseObj, setResponseObj] = useState({});
 
-   return (
-     <div>
-       <h2>Find Current Weather</h2>
-       <div>{JSON.stringify(responseObj)}</div>
-       <button onClick={getForecast}>Search</button>
-     </div>
-   );
-}
+  function getForecast() {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((response) => {
+          console.log(response)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  return (
+    <div className="weatherForcastSection">
+      <h2> Find Current Weather </h2> 
+      <input type="text" className="inputControl" placeholder="Search"/>
+      <div> {JSON.stringify(responseObj)} </div>
+      <button className="searchBtn" onClick={getForecast}> Search </button>
+    </div>
+  );
+};
 
 export default Forecast;
