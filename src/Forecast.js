0ff2b3ed7c88f3bd69aa1e68a78b423fb03fd51f
pras75299 <singh.prashantking@gmail.com>
2020-11-01 11:12:02 +0530

    Ui changes

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,28 +1,36 @@
 import React, { useState } from "react";
-import "./Forecast.css"
-const API_KEY = "e1e3aeef1a428610073ff9107da4bbf5"
-const API_URL = `https://pro.openweathermap.org/data/2.5/forecast/climate?q={London},{GB}&appid=${API_KEY}`
+import "./Forecast.css";
 
 const Forecast = () => {
-  let [responseObj, setResponseObj] = useState({});
-
-  function getForecast() {
-    fetch(API_URL)
-      .then((response) => response.json())
-      .then((response) => {
-          console.log(response)
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-
   return (
     <div className="weatherForcastSection">
-      <h2> Find Current Weather </h2> 
-      <input type="text" className="inputControl" placeholder="Search"/>
-      <div> {JSON.stringify(responseObj)} </div>
-      <button className="searchBtn" onClick={getForecast}> Search </button>
+      <div className="forcastInnerSection">
+        <h2> Find Current Weather </h2> 
+        <input type="text" className="inputControl" placeholder="Search"/>
+        <button className="searchBtn"> Search </button>
+      </div>
+
+      <div className="weatherDetails">
+        <p>Height From Sea Level: <span>3000ft.</span></p>
+        <p>Wind Speed: <span>3000ft.</span></p>
+        <p>Rain: <span>3000ft.</span></p>
+        <p>Humidity: <span>3000ft.</span></p>
+        <p>Temperature: <span>3000ft.</span></p>
+      </div>
+
+      <div className="monthForCast">
+        <div className="singleDaydata"></div>
+        <div className="singleDaydata"></div>
+        <div className="singleDaydata"></div>
+        <div className="singleDaydata"></div>
+        <div className="singleDaydata"></div>
+        <div className="singleDaydata"></div>
+        <div className="singleDaydata"></div>
+        <div className="singleDaydata"></div>
+        <div className="singleDaydata"></div>
+        <div className="singleDaydata"></div>
+      </div>
+
     </div>
   );
 };
